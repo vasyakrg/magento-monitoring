96744ceb7cc88f1795630ea4ddd32bea08c09345
Vassiliy Yegorov <vasyakrg@gmail.com>
2021-03-11 17:58:00 +0700

    fix

--- backup-scripts/backup_upload.sh
+++ backup-scripts/backup_upload.sh
@@ -6,12 +6,12 @@ source .env
 IFS=$'\r\n' command eval "DBS=($(cat ${SCRIPT_DIR}/sites.list))"
 
 echo ">>> upload configs nginx" > ${SCRIPT_DIR}/log.b2.out
-/usr/local/bin/b2 sync --keepDays ${B2_KEEP} ${BACKUP_DIR}/configs/ b2://${B2_BACKET}/configs/ >> ${SCRIPT_DIR}/log.b2.out
+/usr/bin/b2 sync --keepDays ${B2_KEEP} ${BACKUP_DIR}/configs/ b2://${B2_BACKET}/configs/ >> ${SCRIPT_DIR}/log.b2.out
 
 for SITE in ${SITES[*]}
   do
     echo ">>> upload files from $SITE"
-    /usr/local/bin/b2 sync --keepDays ${B2_KEEP} ${SITE_BACKUP_DIR}/site/${SITE}/ b2://${B2_BACKET}/site/${SITE}/ >> ${SCRIPT_DIR}/log.b2.out
+    /usr/bin/b2 sync --keepDays ${B2_KEEP} ${SITE_BACKUP_DIR}/site/${SITE}/ b2://${B2_BACKET}/site/${SITE}/ >> ${SCRIPT_DIR}/log.b2.out
   done
 
 IFS=$'\r\n' command eval "DBS=($(cat ${SCRIPT_DIR}/db.list))"
@@ -19,7 +19,7 @@ IFS=$'\r\n' command eval "DBS=($(cat ${SCRIPT_DIR}/db.list))"
 for DB in ${DBS[*]}
   do
     echo ">>> upload db from $DB"
-    /usr/local/bin/b2 sync --keepDays ${B2_KEEP} ${DB_BACKUP_DIR}/site/${DB}/ b2://${B2_BACKET}/site/${DB}/ >> ${SCRIPT_DIR}/log.b2.out
+    /usr/bin/b2 sync --keepDays ${B2_KEEP} ${DB_BACKUP_DIR}/site/${DB}/ b2://${B2_BACKET}/site/${DB}/ >> ${SCRIPT_DIR}/log.b2.out
   done
 
 exit 0
